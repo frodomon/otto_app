771768fdcd241b6279e71e40790f8ad78b1d604d
Alfredo Vasquez <vasquez.alf@gmail.com>
2016-05-22 11:05:33 -0500

    Nueva App con bootstrap y mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OttoApp::Application.config.secret_key_base = '66ff5c83ca7f8e245cadb09f8370ef58dca7c9b444e675304cdebbaff9dcfb23adcd46719a447b36e96df73da1320f14af97c719e7330ff82c0b4b286f799716'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OttoApp::Application.config.session_store :cookie_store, key: '_otto_app_session'
